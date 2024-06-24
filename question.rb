7d4c06d0d94bfce9d5936802cb1121502d42eaaf
Kim O'Dell <kimbo.odell@gmail.com>
2024-06-24 18:58:59 -0400

    Add method to generate math question

--- question.rb
+++ question.rb
@@ -3,4 +3,21 @@
 
 # class: Question
 # attributes: num1, num2, correct answer
-# methods: generate, validate_answer 
+# methods: generate_question, validate_answer 
+
+class Question
+
+  attr_accessor :num1, :num2, :answer, :question
+
+  def initialize
+    @num1 = rand(1...20)
+    @num2 = rand(1...20)
+    @question = "What does #{@num1} plus #{@num2} equal?"
+    @answer = @num1 + @num2
+  end
+
+  def validate_answer(answer)
+    answer == @answer
+  end   
+
+end  

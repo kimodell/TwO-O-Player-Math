6da4a479f7cf18076dc8a01b90bb854b500e554b
Kim O'Dell <kimbo.odell@gmail.com>
2024-06-24 21:08:34 -0400

    Add code to initialize and run the game

--- main.rb
+++ main.rb
@@ -1,6 +1,12 @@
-# initialize players and start the game loop
+# Start the game loop
 
 require "./game"
 require "./player"
 require "./question"
 require "./turn"
+
+player1 = Player.new("Player 1")
+player2 = Player.new("Player 2")
+
+game = Game.new(player1, player2)
+game.start

819feb69e9dad82cb503ace69422b03480784855
Kim O'Dell <kimbo.odell@gmail.com>
2024-06-24 20:51:04 -0400

    Add comments, fix spelling error in alive method

--- player.rb
+++ player.rb
@@ -1,26 +1,26 @@
-#initialize a name and number of lives (3), set lives based on wins or losses, check if player has lost
-
-#class: Player
-#attributes: name, lives, 
-#methods: lose_life, is_alive?, current_score
+# Initialize a name and number of lives (3), set lives if question answered incorrectly, check if player has zero lives left
 
 class Player 
   attr_accessor  :name, :lives, :score
 
+  # Initialize variables for name, lives and current score
   def initialize (name)
     @name = name
     @lives = 3
     @score = 0
   end  
 
-  def is_ailve?
+  # Method to check if player has more and zero lives
+  def alive
     @lives > 0
   end
   
+  # Method to change players number of lives
   def lose_life
     @lives -= 1
   end
 
+  # Method to display players current score
   def current_score
     "#{@lives}/3"
   end  

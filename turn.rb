ffd984106575f3738feeab9c0a9c95010ff543a8
Kim O'Dell <kimbo.odell@gmail.com>
2024-06-24 20:55:54 -0400

    Add class to manage logic for single turn

--- turn.rb
+++ turn.rb
@@ -1,9 +1,29 @@
 # Manage the logic for a single turn.
-# Interact with the player to ask a question and validate their answer.
+# Interact with the player by asking a question and checking their answer.
 # Update the player's state (lives) based on their answer.
 # Manages user input
 
+class Turn 
+  attr_accessor :player, :question
 
-# Class: Turn
-# Attributes: player, question, is_correct
-# Methods: generate_question, ask_question, check_answer
+  # Initalize player and question variables
+  def initialize(player)
+    @player = player
+    @question = Question.new
+  end
+
+  # Method to display question, handle user input and determine if user input is the correct answer/check validation.
+  # Displays message based on result of validation check.
+  def play
+    puts "---- NEW TURN ----"
+    puts "#{player.name}: #{@question.question}"
+    print "> "
+    answer = gets.chomp.to_i
+    if @question.validate_answer(answer)
+      puts "#{player.name}: YES! That is correct!"
+    else
+      puts "#{player.name}: Sorry, that is incorrect!"
+      player.lose_life
+    end
+  end
+end  
